b7b1b678152208c90a18d2ef00bdf71424829c73
T5000 <tomislav13@protonmail.com>
2021-01-11 22:44:49 +0100

    Update MessageDetails.cshtml.cs

--- Controllers/MessageDetails.cshtml.cs
+++ Controllers/MessageDetails.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+using System.Text.Json;                                                                //11.01.2021 TMLINARIC Needed for json parsing
 using Turtl_TMlinaric.Models;                                                          //10.01.2021 TMLINARIC References the definition of "Message" element
 using Turtl_TMlinaric.Shared;                                                          //11.01.2021 TMLINARIC Code repetition is bad, m'kaaaaay?
 
@@ -22,11 +23,20 @@ namespace Turtl_TMlinaric.Pages
         }
 
         public int messageID { get; set; }
+        public List<Message> listOfMessages { get; set; }
 
-        public IActionResult OnGet(int id)
+        public async Task<IActionResult> OnGet(int id)
         {
-            string stringJsonMessages = Functions.GetJsonMessagesFromUrlAsString("https://jsonplaceholder.typicode.com/posts");
-            
+            messageID = id;
+            string stringJsonMessages = await Functions.GetJsonMessagesFromUrlAsString("https://jsonplaceholder.typicode.com/posts");
+
+            if (stringJsonMessages != null)
+                listOfMessages = JsonSerializer.Deserialize<List<Message>>(stringJsonMessages);
+                
+            else
+            {
+                //TODO LOG ERROR
+            }
 
             return Page();
         }
